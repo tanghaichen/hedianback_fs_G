6e8234abea5620a3cb4a9499d73681f7a3e2444f
tanghaichen <964277403@qq.com>
2019-09-28 12:30:06 +0800

    3

--- src/main/java/net/cobona/vici/controller/computeYSD/linuxDemo.java
+++ src/main/java/net/cobona/vici/controller/computeYSD/linuxDemo.java
@@ -20,7 +20,7 @@ public class linuxDemo {
 	
 	
 	public static void main(String[] args) throws Exception {
-		double fs=1;
+		double fs=13;
 
 final ParameterTool params = ParameterTool.fromArgs(args);
 		//获得属性
